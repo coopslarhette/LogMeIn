0c7b9ba5cf239045669b747e8bc4340edb2bd66b
coopslarhette <coopslarhette@gmail.com>
2018-12-26 11:13:32 -0800

    rearranges structure

--- Main.py
+++ Main.py
@@ -1,10 +1,4 @@
-# prolly should use MySql
-import sqlite3
-
 credentials = {}
-
-conn = sqlite3.connect('/Users/coopslarhette/PycharmProjects/LogMeIn')
-curs = conn.cursor()
 
 
 def has_user(user):
@@ -15,27 +9,28 @@ def create_new_account(user, password):
     while has_user(user):
         user = input("Please enter a new username, that one is taken.")
     credentials[user] = password
-    print(credentials[user])
 
 
-class LogMeIn():
-    userInput = input("Please enter username, or enter 'new account' if you wish to register: ")
-    loggedIn = False
-    if userInput.lower() == "new account":
-        userInput = input("Please enter a username: ")
-        pw = input("Please enter a password: ")
-        # creates account later to not get asked twice
-    elif has_user(userInput):
-        pw = input("Pleas enter the password for your account: ")
-    else:
-        print("Their is no such registered user, please come again.")
-        exit(0)
-    create_new_account(userInput, pw)
+def log_in(username, password):
     i = 0
     while i < 4 and not loggedIn:
-        if credentials[userInput] == pw:
+        if credentials[username] == pw:
             loggedIn = True
             print("Congrats you're logged in!")
         else:
             pw = input("Password inccorect, please enter a new password: ")
         i += 1
+
+
+class LogMeIn:
+    username = ""
+    userInput = input("Please enter username, or enter 'new account' if you wish to register: ")
+    if userInput.lower() == "new account":
+        userNameInput = input("Please enter a username: ")
+        passInput = input("Please enter a password: ")
+        create_new_account(userInput, passInput)
+    if has_user(userInput):
+        pw = input("Pleas enter the password for your account: ")
+    else:
+        print("Their is no such registered user, please come again.")
+        exit(0)
